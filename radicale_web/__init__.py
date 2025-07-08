541f1dff495b2605b52e9739c6ba7abc6d92bd9a
Dominic Moser <dominic@dmoser.com>
2025-07-09 01:05:52 +0200

    feat: add normal radicale web plugin

---
+++ radicale_web/__init__.py
@@ -0,0 +1,35 @@
+# This file is part of Radicale - CalDAV and CardDAV server
+# Copyright © 2017-2018 Unrud <unrud@outlook.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The web module for the website at ``/.web``.
+
+Take a look at the class ``BaseWeb`` if you want to implement your own.
+
+"""
+
+
+from radicale import httputils, types, web
+
+MIMETYPES = httputils.MIMETYPES  # deprecated
+FALLBACK_MIMETYPE = httputils.FALLBACK_MIMETYPE  # deprecated
+
+class Web(web.BaseWeb):
+
+    def get(self, environ: types.WSGIEnviron, base_prefix: str, path: str,
+            user: str) -> types.WSGIResponse:
+        return httputils.serve_resource("radicale.web", "internal_data",
+                                        base_prefix, path)
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+
+from setuptools import setup
+
+VERSION = "3.1.6"
+
+package_path = os.path.join(os.path.dirname(__file__), "radicale_web")
+web_data = sum((
+    [os.path.relpath(os.path.join(root, f), package_path)
+     for f in files if not f.startswith(".") and not f.endswith("~")]
+    for root, _, files in os.walk(os.path.join(package_path, "web"))), [])
+
+setup(
+    name="Radicale_Web",
+    version=VERSION,
+    description="Radicale Web without logging in",
+    author="Dominic",
+    author_email="dominic@dmoser.dev",
+    url="https://github.com/DominicMoser/radicale-plugin-web-x-remote-user",
+    license="GNU GPL v3",
+    platforms="Any",
+    packages=["radicale_web"],
+    package_data={"radicale_web": web_data},
+    install_requires=["radicale>=3.4.0"])
